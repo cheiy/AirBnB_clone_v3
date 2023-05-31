aa6ea626f003fffcee3c3fd6957d111108025533
mwaskagi <gituiku.mwangi@gmail.com>
2023-05-31 07:09:07 -0700

    File

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -71,6 +71,13 @@ class FileStorage:
 
     def get(self, cls, id):
         """  Return class name and its ID or None if not found """
+        obj_dict = self.all(cls)
+        for k, v in obj_dict.items():
+            match = cls + '.' + id
+            if k == match:
+                return v
+
+        return None
 
     def count(self, cls=None):
         """  Returns number of objects in storage """
